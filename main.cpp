ea9ad83971dc70aaca73dcd7e22320fbcd7c123f
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-02-21 01:31:23 +0200

    Update main.cpp
    iterptas medianos radimas

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 struct studentas {
     string vardas;
     string pavarde;
-    double n; // pazymiu skaicius
+    double n;// pazymiu skaicius
     double vidurkis = 0;
-    int med;
+    double med=0;
     double egz;
     double gal;
 };
@@ -18,25 +18,38 @@ struct studentas {
 studentas rezult;
 
 void as() {
-    double nd; //pazymys
+    double nd;//pazymys
     cout << "iveskite varda ";
     cin >> rezult.vardas;
     cout << "iveskite pavarde ";
     cin >> rezult.pavarde;
     cout << "iveskite kiek pazymiu ";
     cin >> rezult.n;
-    for (int i = 0; i < rezult.n; i++) { // iveda visus skaicius
+    int t = rezult.n;//laiko n verte kaip int
+    for (int i = 0; i < rezult.n; i++) {// iveda visus skaicius + randa mediana
         cout << "iveskite pazymi ";
         cin >> nd;
         rezult.vidurkis += nd;
-    } 
+        
+        if (t % 2 == 1) {
+            if (i == t / 2) {
+                rezult.med = nd;
+            }
+        }
+        else {
+            if (i+1 == t / 2 || i==t/2) {
+                rezult.med += nd/2;
+            }
+        }
+        
+    }
     rezult.vidurkis = rezult.vidurkis / rezult.n;
     cout << "iveskite egzamino rezultata ";
     cin >> rezult.egz;
+    cout << "Pavarde     Vardas     Galutinis(vid.) / Galutinis(med.)" << endl;
+    cout << "-----------------------------" << endl;
     rezult.gal = 0.6 * rezult.egz + 0.4 * rezult.vidurkis;
-    cout << "Pavarde     Vardas     Galutinis(vid.)" << endl;
-    cout << "-----------------------------"<<endl;
-    cout << rezult.vardas << "     " << rezult.pavarde << "     " << rezult.gal;
+    cout << rezult.vardas << "     " << rezult.pavarde << "     " << rezult.gal << "     " << rezult.med << endl;
 }
 
 
@@ -46,4 +59,6 @@ int main() {
     for (int i = 0; i < sk; i++) {
         as();
     }
+    
+    
 }
