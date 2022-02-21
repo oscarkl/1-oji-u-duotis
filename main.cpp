8197743f53c0c517b5b2e4e0c5a623af26456797
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-02-21 03:35:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <math.h>
+#include <stdlib.h>
+#include <string>
 #include <vector>  
 int sk;
 
@@ -8,57 +9,74 @@ using namespace std;
 struct studentas {
     string vardas;
     string pavarde;
-    double n;// pazymiu skaicius
     double vidurkis = 0;
-    double med=0;
-    double egz;
-    double gal;
+    double med, gal, galm;
+    vector<int> n;
 };
 
-studentas rezult;
+bool isNumber(const string& str)
+{
+    for (char const& c : str) {
+        if (std::isdigit(c) == 0) return false;
+    }
+    return true;
+}
+vector<studentas> rezult;
 
-void as() {
-    double nd;//pazymys
-    cout << "iveskite varda ";
-    cin >> rezult.vardas;
-    cout << "iveskite pavarde ";
-    cin >> rezult.pavarde;
-    cout << "iveskite kiek pazymiu ";
-    cin >> rezult.n;
-    int t = rezult.n;//laiko n verte kaip int
-    for (int i = 0; i < rezult.n; i++) {// iveda visus skaicius + randa mediana
-        cout << "iveskite pazymi ";
+void ivestis(int i, int max) {
+    string pavard;
+    string nd;
+    int sk;
+    bool g = true;
+    cin >> pavard;
+    rezult[i].pavarde = pavard;
+    while (g) {
         cin >> nd;
-        rezult.vidurkis += nd;
-        
-        if (t % 2 == 1) {
-            if (i == t / 2) {
-                rezult.med = nd;
-            }
+        if (isNumber(nd)) {
+            sk = stoi(nd);
+            rezult[i].n.push_back(sk);
+            rezult[i].vidurkis += sk;
         }
         else {
-            if (i+1 == t / 2 || i==t/2) {
-                rezult.med += nd/2;
+            g = false;
+            
+            if (i + 1 < max) {
+                rezult[i + 1].vardas = nd;
             }
         }
-        
     }
-    rezult.vidurkis = rezult.vidurkis / rezult.n;
-    cout << "iveskite egzamino rezultata ";
-    cin >> rezult.egz;
-    cout << "Pavarde     Vardas     Galutinis(vid.) / Galutinis(med.)" << endl;
-    cout << "-----------------------------" << endl;
-    rezult.gal = 0.6 * rezult.egz + 0.4 * rezult.vidurkis;
-    cout << rezult.vardas << "     " << rezult.pavarde << "     " << rezult.gal << "     " << rezult.med << endl;
+}
+void medi(int i, int j) {
+    if (j % 2 == 1) {
+       rezult[i].med = rezult[i].n[j / 2 + 1];
+    }
+    else {
+        rezult[i].med = (rezult[i].n[j / 2] + rezult[i].n[j / 2 + 1]) / 2;
+    }
+}
+
+void galVid(int i) {
+    rezult[i].gal = 0.6 * rezult[i].n.back() + 0.4 * rezult[i].vidurkis;
+    rezult[i].galm = 0.6 * rezult[i].med + 0.4 * rezult[i].vidurkis;
 }
 
 
 int main() {
-    cout << "kiek studentu ";
-    cin >> sk;
-    for (int i = 0; i < sk; i++) {
-        as();
+    int studentSk = rand() % 3 + 1;
+    cout << studentSk;
+    for (int i = 0; i < studentSk; i++) {
+        rezult.push_back(studentas());
     }
-    
-    
+    cin >> rezult[0].vardas;
+    for (int i = 0; i < studentSk; i++) {
+        ivestis(i, studentSk);
+        rezult[i].vidurkis = (rezult[i].vidurkis - rezult[i].n.back()) / rezult[i].n.size();
+        medi(i, rezult[i].n.size());
+        galVid(i);
+    }
+    cout << "Pavarde     Vardas     Galutinis(vid.) / Galutinis(med.)" << endl;
+    cout << "-----------------------------" << endl;
+    for (int i = 0; i < studentSk; i++) {
+        cout << rezult[i].vardas << "     " << rezult[i].pavarde << "     " << rezult[i].gal << "     " << rezult[i].galm << endl;
+    }
 }
