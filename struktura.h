16361a13621d96fb6a5f4d379ed523cc7c7ae73c
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-03-07 02:57:14 +0200

    Create struktura.h
    laiko strukturą

---
+++ struktura.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <vector>
+struct studentas {
+    std::string vardas;
+    std::string pavarde;
+    double vidurkis = 0;
+    double med, gal, galm;
+    std::vector<int> n;
+};
