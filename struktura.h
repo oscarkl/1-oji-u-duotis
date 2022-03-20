beecba18e9b31d1839bb4e50a3e2ac70a0656028
oscarkl <42514691+oscarkl@users.noreply.github.com>
2022-03-20 23:55:00 +0200

    Update struktura.h

--- struktura.h
+++ struktura.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <iostream>
-#include <vector>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <time.h> 
+#include <string>
+#include <vector>  
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <chrono>
 struct studentas {
     std::string vardas;
     std::string pavarde;
